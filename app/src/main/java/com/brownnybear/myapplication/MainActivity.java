f27d79062ae8ed391136a68c7a1914be7f032944
thislovez <g-o-d_god@hotmail.com>
2018-03-02 11:55:07 +0700

    1 commit

---
+++ app/src/main/java/com/brownnybear/myapplication/MainActivity.java
@@ -0,0 +1,57 @@
+package com.brownnybear.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onCheckboxClicked(View view) {
+
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        switch (view.getId()) {
+            case R.id.checkbox_meat:
+                if (checked)
+                    System.out.println("Put some meat on the sandwich");
+                else
+                    System.out.println("Remove the meat");
+                break;
+            case R.id.checkbox_cheese:
+                if (checked)
+                    System.out.println("Cheese me");
+                else
+                    System.out.println("I'm lactose intolerant");
+                break;
+
+        }
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_pirates:
+                if (checked)
+                    System.out.println("Pirates are the best");
+                break;
+            case R.id.radio_ninjas:
+                if (checked)
+                    System.out.println("Ninjas rule ");
+                break;
+        }
+    }
+
+
+}
